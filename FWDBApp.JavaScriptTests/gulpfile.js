180114cd0128fa0b950ae28b0ef3cc8f946157e9
ayush.shukla <ayush.shukla@RINPC157>
2016-07-11 08:21:08 +0530

    adding javascript test

---
+++ FWDBApp.JavaScriptTests/gulpfile.js
@@ -0,0 +1,12 @@
+﻿var gulp = require('gulp');
+var mocha = require('gulp-mocha');
+var supertest = require("supertest");
+var should = require("should");
+
+
+gulp.task('mocha', function () {
+    return gulp.src(['javaScriptTest.js'], { read: false })
+      .pipe(mocha({
+          reporter: 'spec'
+      }));
+});
---
+++ FWDBApp.JavaScriptTests/javascriptTest.js
@@ -0,0 +1,29 @@
+﻿var supertest = require("supertest");
+var should = require("should");
+
+var server = supertest.agent("http://localhost:65247");
+
+describe("javascript test", function () {
+    it("should pass if output static path returns", function (done) {
+        server
+        .get("/#/Default/Static")
+        .expect("Content-type", /json/)
+        .expect(200) // THis is HTTP response
+        .end(function (err, res)
+        {
+            res.status.should.equal(200);
+            done();
+        });
+    });
+
+    it("should pass if output ajax path returns", function (done) {
+        server
+        .get("/#/Default/Ajax")
+        .expect("Content-type", /json/)
+        .expect(200) // THis is HTTP response
+        .end(function (err, res) {
+            res.status.should.equal(200);
+            done();
+        });
+    });
+});
